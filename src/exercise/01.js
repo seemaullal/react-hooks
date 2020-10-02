9ec74e5b6891459050eee168c01e9c49acd35828
Seema Ullal <seema.ullal@gusto.com>
2020-10-01 23:46:03 -0700

    inline exercise 1 function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -6,15 +6,16 @@ import React from 'react';
 function Greeting({initialName}) {
   const [name, setName] = React.useState(initialName || '');
 
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input
+          onChange={event => {
+            setName(event.target.value);
+          }}
+          id="name"
+        />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
